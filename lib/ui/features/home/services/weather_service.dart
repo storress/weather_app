2ca8e5a99aa4ccc2b1f589b74138f03f543801de
Salomon Torres <salomontorres@MacBook-Pro-de-Salo.local>
2026-01-15 20:21:55 -0300

    Remove unused function

--- lib/ui/features/home/services/weather_service.dart
+++ lib/ui/features/home/services/weather_service.dart
@@ -7,23 +7,6 @@ class WeatherService {
   static const String _baseUrl = 'https://api.openweathermap.org/data/2.5';
   static const String _apiKey = 'fb7a15194674e4cc231ee9ba5df957f4'; // Replace with your actual API key
 
-  Future<Map<String, dynamic>> getCurrentWeather(String city) async {
-    final url = Uri.parse('$_baseUrl/weather?q=$city&appid=$_apiKey&units=metric');
-
-    try {
-      final response = await http.get(url);
-
-      if (response.statusCode == 200) {
-        print(response.body);
-        return json.decode(response.body);
-      } else {
-        throw Exception('Failed to load weather data: ${response.statusCode}');
-      }
-    } catch (e) {
-      throw Exception('Error fetching weather: $e');
-    }
-  }
-
   Future<ForecastDtoModel> getForecast(String city) async {
     final url = Uri.parse('$_baseUrl/forecast?q=$city&appid=$_apiKey&units=metric');
 
